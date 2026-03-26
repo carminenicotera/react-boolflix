7f4e594f7b4bd22d46815fb6e60d4be1f4bfc67a
carminenicotera <carmine.91.n@hotmail.it>
2026-03-26 13:04:40 +0100

    Add initial structure of the searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-
+import { useState } from "react";
 
 function App() {
 
   const API_KEY = import.meta.env.VITE_THE_MOVIE_DB_API_KEY
-  console.log(API_KEY);
+  //console.log(API_KEY);
 
   const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
-  console.log(api_url);
-  
+  //console.log(api_url);
+
+  const [searchBar, setSearcBar] = useState('')
+
+  function handleSearch(e) {
+    //console.log(e)
+    setSearcBar(e.target.value)
+  }
+
+  function handleButton() {
+    console.log('stai cercando');
+  }
+
   return (
     <>
-
+      <h1>Boolflix</h1>
+      <input type="text" value={ searchBar } placeholder="type a movie" onChange={ handleSearch } />
+      <button onClick={ handleButton }>search</button>
     </>
   )
 }
