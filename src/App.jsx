2494f1a0ee18e95e4012a0385d59a344063775d8
carminenicotera <carmine.91.n@hotmail.it>
2026-03-26 21:21:30 +0100

    Refactor search functionality to combine movie and TV show results

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,58 @@ import axios from "axios";
 
 const api_url = `https://api.themoviedb.org`
 
-
+const flags = {
+  it: "/Flag_of_Italy.svg.png",
+  en: "/Flag_of_the_United_Kingdom.svg.png",
+  fr: "/Flag_of_France.svg.webp",
+  es: "/Flag_of_Spain.svg.png",
+}
 
 function App() {
 
   const API_KEY = import.meta.env.VITE_THE_MOVIE_DB_API_KEY
   //console.log(API_KEY);
 
-  const flags = {
-    it: "/public/Flag_of_Italy.svg.png",
-    en: "/public/Flag_of_the_United_Kingdom.svg.png",
-    fr: "/public/Flag_of_France.svg.webp",
-    es: "/public/Flag_of_Spain.svg.png",
-  }
-
   const [searchBar, setSearcBar] = useState('')
-  const [movies, setMovies] = useState([])
+  const [results, setResults] = useState([])
 
+  // Aggiorno stato dell'input
   function handleSearch(e) {
     //console.log(e)
     setSearcBar(e.target.value)
   }
 
+  // Al clic eseguo due chiamate per film e serie
   function handleButton() {
-    //console.log('stai cercando');
-    axios(`${api_url}/3/search/movie?api_key=${API_KEY}&query=${searchBar}`)
-      .then(res => {
-        //console.log(res.data);
-        setMovies(res.data.results)
-        //console.log(res.data.results);
+
+    axios.get(`${api_url}/3/search/movie?api_key=${API_KEY}&query=${searchBar}`)
+      .then(resMovie => {
+        const movieFound = resMovie.data.results
+
+        axios.get(`${api_url}/3/search/tv?api_key=${API_KEY}&query=${searchBar}`)
+          .then(resTv => {
+            const tvFound = resTv.data.results
+
+            // Utilizzo lo spread operator per estrarre gli elementi dai due array ricevuti dalle API e inserirli in un nuovo array
+            setResults([...movieFound, ...tvFound])
+          })
       })
   }
 
   return (
     <>
       <h1>Boolflix</h1>
-      <input type="text" value={ searchBar } placeholder="type a movie" onChange={ handleSearch } />
+      <input type="text" value={ searchBar } placeholder="type a movie or a series" onChange={ handleSearch } />
       <button onClick={ handleButton }>search</button>
 
       <ul>
-        { movies.map(movie =>
-          <li key={ movie.id }>
-            Titolo:{ movie.title }
-            <div>Titolo originale:{ movie.original_title }</div>
-            Lingua:{ flags[movie.original_language] ? (<img className="flagImage" src={ flags[movie.original_language] } alt="language" />) : (<span> { movie.original_language }</span>) }
-            <div>Voto:{ movie.vote_average }</div>
+        { results.map(result =>
+          <li key={ result.id }>
+            Titolo:{ result.title }
+            <div>Titolo originale:{ result.original_title }</div>
+            {/* Se la lingua è presente nell'oggetto flags mostra la bandiera altrimenti mostra solo le iniziali della lingua */ }
+            Lingua:{ flags[result.original_language] ? (<img className="flagImage" src={ flags[result.original_language] } alt="language" />) : (<span> { result.original_language }</span>) }
+            <div>Voto:{ result.vote_average }</div>
           </li>) }
       </ul>
     </>
