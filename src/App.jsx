04ee2428f67c02c73270399d8528c4f2dcf725ee
carminenicotera <carmine.91.n@hotmail.it>
2026-03-26 12:07:25 +0100

    Add API key and example .env file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@
 
 function App() {
 
+  const API_KEY = import.meta.env.VITE_THE_MOVIE_DB_API_KEY
+  console.log(API_KEY);
+
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
+  console.log(api_url);
+  
   return (
     <>
-      
+
     </>
   )
 }
