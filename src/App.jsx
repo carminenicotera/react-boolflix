4d063c7987dd7ebbc293f315e34b137b1cadc67c
carminenicotera <carmine.91.n@hotmail.it>
2026-03-26 13:29:43 +0100

    Implement movie search using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import axios from "axios";
+
+const api_url = `https://api.themoviedb.org`
 
 function App() {
 
   const API_KEY = import.meta.env.VITE_THE_MOVIE_DB_API_KEY
   //console.log(API_KEY);
 
-  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
-  //console.log(api_url);
 
   const [searchBar, setSearcBar] = useState('')
+  const [movies, setMovies] = useState([])
 
   function handleSearch(e) {
     //console.log(e)
@@ -16,7 +18,13 @@ function App() {
   }
 
   function handleButton() {
-    console.log('stai cercando');
+    //console.log('stai cercando');
+    axios(`${api_url}/3/search/movie?api_key=${API_KEY}&query=${searchBar}`)
+      .then(res => {
+        console.log(res.data);
+        setMovies(res.data.results)
+        console.log(res.data.results);
+      })
   }
 
   return (
@@ -24,6 +32,10 @@ function App() {
       <h1>Boolflix</h1>
       <input type="text" value={ searchBar } placeholder="type a movie" onChange={ handleSearch } />
       <button onClick={ handleButton }>search</button>
+
+      <ul>
+        { movies.map(movie => <li key={ movie.id }>{ movie.title }</li>) }
+      </ul>
     </>
   )
 }
